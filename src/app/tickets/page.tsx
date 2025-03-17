f71b7db3131b91a65a87b95ec8d10420aedbc9af
hossidev <hossi@hossi.is>
2025-03-17 15:12:51 +0000

    Add react-error-boundary package and update ticket pages
    - Added `react-error-boundary` dependency to package.json and package-lock.json for improved error handling in React components.
    - Refactored TicketPage to utilize `notFound` for handling missing tickets, enhancing user experience.
    - Minor formatting adjustment in TicketsPage for better code readability.

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import { Heading } from "@/components/heading";
 import { Spinner } from "@/components/spinner";
 import { TicketList } from "@/features/ticket/components/ticket-list";
+
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
